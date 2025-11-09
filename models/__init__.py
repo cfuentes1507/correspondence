48b7910249cadbb3a5ba9969332990a83681e23a
CFuentes.Dev <151073430+cfuentes1507@users.noreply.github.com>
2025-11-09 02:16:54 -0400

    Fix

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,9 @@
 
 from . import correspondence_document_read_status
 from . import correspondence_department_director
+from . import res_partner
+from . import res_users
 from . import correspondence_department_correlative
 from . import correspondence_department
 from . import correspondence_type
 from . import correspondence_document
-from . import res_users
-from . import res_partner
