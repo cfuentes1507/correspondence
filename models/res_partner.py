7796ba84eb9c32985b69493bc90f4cee0956e167
CFuentes.Dev <c2003fuentes@gmail.com>
2025-11-10 14:16:53 -0400

    Campos firma y sello

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,3 +6,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     director_description = fields.Html(string="Descripción del Cargo")
+    director_signature = fields.Binary(string="Firma del Director", attachment=True)
+    director_stamp = fields.Binary(string="Sello del Director", attachment=True)
